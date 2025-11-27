5e8e30393799385258e35ad41db6f52058c04dd8
sufiyansharfudheen-cell <sufiyansharfudheen@gmail.com>
2025-11-27 15:52:14 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import Home from './componenets/Home'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-<<<<<<< HEAD
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-=======
 import About from './componenets/About'
 import Contact from './componenets/Contact'
->>>>>>> 40a85c609c8ca4887f9d21b0f16d8a87fcd1e2e5
 
 function App() {
  

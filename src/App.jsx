a0cf499f8766a9f66e969b60cc9e68a8a9eb0ad9
Nishanthnithu <nishanthnithu36@gmail.com>
2025-11-28 14:49:36 +0530

    jkadskfj

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import About from './componenets/About'
 import Contact from './componenets/Contact'
-import Reviews from './componenets/Review'
 import Shop from './componenets/Shop'
 
 
@@ -19,7 +18,7 @@ function App() {
               <Route path='/Shop'element={<Shop/>}/>
                <Route path="/About" element={<About />}/>
                <Route path="/Contact" element={<Contact />}/>
-               <Route path="/Review" element={<Reviews />}/>
+               
              
               </Routes>
       </BrowserRouter>
